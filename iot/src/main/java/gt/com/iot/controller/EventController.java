8248ded958de2aef16fa513cbd4cbd3455f875c7
Marvin <malopez@takitosu.localdomain>
2021-09-25 01:51:02 -0600

    Agregando controladores

---
+++ iot/src/main/java/gt/com/iot/controller/EventController.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gt.com.iot.controller;
+
+import gt.com.iot.model.Event;
+import gt.com.iot.service.EventService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author malopez
+ */
+@RestController
+public class EventController {
+
+    @Autowired
+    private EventService eventService;
+
+    @GetMapping("/event")
+    List<Event> getEventList() {
+        return eventService.getAll();
+    }
+
+    @GetMapping("/event/sensor/{id}")
+    List<Event> getEventListBySensor(@PathVariable Integer sensorId) {
+        return eventService.getEventsBySensor(sensorId);
+    }
+
+    @GetMapping("/event/{id}")
+    Event getEvent(@PathVariable Integer id) {
+        return eventService.getEventByID(id);
+    }
+
+    @PostMapping("/event")
+    Event createEvent(@RequestBody Event event) {
+        return eventService.createEvent(event);
+    }
+
+    @PutMapping("/event/{id}")
+    Event updateEvent(@RequestBody Event event, @PathVariable Integer id) {
+        Event oldEvent = eventService.getEventByID(id);
+        oldEvent.setEventCode(event.getEventCode());
+        oldEvent.setTimeEvent(event.getTimeEvent());
+        oldEvent.setSensor(event.getSensor());
+        return eventService.updateEvent(oldEvent);
+    }
+
+    @DeleteMapping("/event/{id}")
+    void deleteEvent(@PathVariable Event event) {
+        eventService.deleteEvent(event);
+    }
+}
